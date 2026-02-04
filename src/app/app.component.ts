c61b5541e22a284f924688de0c8a6d146a274b3d
unknown <rishishri07@gmail.com>
2026-02-04 17:23:04 +0530

    Initial anniversary website 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'anniversary-website';
+  activePageIdx = 0;
+  collageList = [
+    {page: 0, url: 'assets/images/iti.jpg', class: 'pos-1'},
+    {page: 0, url: 'assets/images/cover_min.jpg', class: 'pos-2'},
+    {page: 0, url: 'assets/images/rishi.jpg', class: 'pos-3'}
+  ]
+
+  goToPage(idx: number) {
+    if (idx === 0) {
+      this.collageList = [
+        {page: 0, url: 'assets/images/iti.jpg', class: 'pos-1'},
+        {page: 0, url: 'assets/images/cover_min.jpg', class: 'pos-2'},
+        {page: 0, url: 'assets/images/rishi.jpg', class: 'pos-3'}
+      ];
+      this.activePageIdx = 0;
+    } else if (idx === 1) {
+      this.collageList = [
+        {page: 0, url: 'assets/images/iti.jpg', class: 'pos-1'},
+        {page: 0, url: 'assets/images/cover_min.jpg', class: 'pos-2'},
+        {page: 0, url: 'assets/images/rishi.jpg', class: 'pos-3'}
+      ];
+      this.activePageIdx = 1;
+    } else if (idx === 2) {
+      this.collageList = [
+        {page: 0, url: 'assets/images/iti.jpg', class: 'pos-1'},
+        {page: 0, url: 'assets/images/cover_min.jpg', class: 'pos-2'},
+        {page: 0, url: 'assets/images/rishi.jpg', class: 'pos-3'}
+      ];
+      this.activePageIdx = 2;
+    }
+  }
 }
